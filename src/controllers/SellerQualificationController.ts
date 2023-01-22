c49ed6be5a43691e38110d55d83ceeb93e1d7d80
CarlosGarzon <carlosangarzon@uan.edu.co>
2023-01-22 11:53:11 -0500

    TC 22 : add seller qualification

--- src/controllers/SellerQualificationController.ts
+++ src/controllers/SellerQualificationController.ts
@@ -5,6 +5,7 @@ import HTTP_STATUS_CODES from 'http-status';
 import { DebugUtilities } from '../utilities/DebugUtilities';
 import { SellerQualificationService } from '../services/SellerQualificationService';
 
+
 const debug = debugLib('tc:SellerQualificationController');
 const SellerQualificationController = Router();
 
@@ -27,8 +28,21 @@ SellerQualificationController.get(
 );
 
 //add seller rating
-SellerQualificationController.post('/Seller/addSellerQualification',function(req,res){
-    console.log('Agregar vendedor')
-});
+SellerQualificationController.post(
+    '/Seller/addSellerQualification',
+    RequestLogger.basic,
+    async (req: Request, res: Response) => {
+        try {
+            const response =  await SellerQualificationService.addSellerQualification(req.body);
+            res.status(HTTP_STATUS_CODES.OK).send(response);
+        } catch (err) {
+            const error = DebugUtilities.error(err, 'Error');
+            debug('ERROR: POST-CoeController: %j', error.statusError);
+            res.status(error.codeStatusError).send(error.statusError);
+        }
+    }
+);
+
+
 
 export default SellerQualificationController;
--- src/services/SellerQualificationService.ts
+++ src/services/SellerQualificationService.ts
@@ -1,6 +1,11 @@
 import debugLib from 'debug';
 import SellerQualificationDataSource from '../datasource/SellerQualificationDataSource';
+//model
 import { IBuyerQualificationResponse } from '../model/Response/IBuyerQualificationResponse';
+import { ISellerAddQualificationResponse } from '../model/Response/ISellerAddQualificationResponse';
+import { ISellerAddQualificationRequest } from '../model/Request/ISellerAddQualificationRequest';
+
+
 
 const debug = debugLib('tc:SellerQualificationService');
 
@@ -16,4 +21,14 @@ export class SellerQualificationService {
         return Promise.reject(err);
      }
     }
+
+    public static async addSellerQualification(dataRequest : ISellerAddQualificationRequest): Promise<ISellerAddQualificationResponse> {
+      try {
+         const response =  await SellerQualificationDataSource.addSellerQualification(dataRequest);
+         return Promise.resolve(response);
+      } catch (err) {
+         debug('Error trying to obtain the search configuration %s ', err);
+         return Promise.reject(err);
+      }
+     }
 }
