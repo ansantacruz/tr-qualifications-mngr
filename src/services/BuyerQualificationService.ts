84a67b8db2eae4c12057248d4bff0712747d5114
Andres camilo Santacruz Borda <31628943+ansantacruz@users.noreply.github.com>
2023-02-25 22:01:45 -0500

    TC-39: I have fixed test

--- src/services/BuyerQualificationService.ts
+++ src/services/BuyerQualificationService.ts
@@ -2,7 +2,7 @@ import debugLib from 'debug';
 import BuyerQualificationDataSource from '../datasource/BuyerQualificationDataSource';
 import { IBuyerQualificationResponse } from '../model/Response/IBuyerQualificationResponse';
 //Model
-import {IBuyyerAddQualificationRequest } from '../model/Request/IBuyerAddQualificationRequest';
+import { IBuyyerAddQualificationRequest } from '../model/Request/IBuyerAddQualificationRequest';
 import { IBuyerAddQualificationResponse } from '../model/Response/IBuyerAddQualificationResponse';
 
 const debug = debugLib('tc:BuyerQualificationService');
@@ -10,24 +10,25 @@ const debug = debugLib('tc:BuyerQualificationService');
 
 export class BuyerQualificationService {
 
-    public static async getBuyerQualification(idBuyer : number): Promise<IBuyerQualificationResponse> {
-     try {
-        const response =  await BuyerQualificationDataSource.getBuyerQualification(idBuyer);
-        return Promise.resolve(response);
-     } catch (err) {
-        debug('Error trying to obtain the search configuration %s ', err);
-        return Promise.reject(err);
-     }
-    }
-
-    public static async addBuyerQualification(dataRequest : IBuyyerAddQualificationRequest): Promise<IBuyerAddQualificationResponse> {
+   public static async getBuyerQualification(idBuyer: number): Promise<IBuyerQualificationResponse> {
       try {
-         const response =  await BuyerQualificationDataSource.addBuyerQualification(dataRequest);
+         const response = await BuyerQualificationDataSource.getBuyerQualification(idBuyer);
          return Promise.resolve(response);
       } catch (err) {
          debug('Error trying to obtain the search configuration %s ', err);
          return Promise.reject(err);
       }
-     }
-    
+   }
+
+   public static async addBuyerQualification(dataRequest: IBuyyerAddQualificationRequest):
+   Promise<IBuyerAddQualificationResponse> {
+      try {
+         const response = await BuyerQualificationDataSource.addBuyerQualification(dataRequest);
+         return Promise.resolve(response);
+      } catch (err) {
+         debug('Error trying to obtain the search configuration %s ', err);
+         return Promise.reject(err);
+      }
+
+   }
 }
